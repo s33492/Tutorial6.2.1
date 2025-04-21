06c405a12ccecd556d6bd5c53e5660aae2e6b1c7
s33492 <s33492@pjwstk.edu.pl>
2025-04-21 18:15:06 +0200

    Update Car.java

--- src/Car.java
+++ src/Car.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
-
-public class Car {
+class Car extends Vehicle {
+    private int numberOfSeats;
+    public Car(int numberOfSeats) {
+        this.numberOfSeats = numberOfSeats;
+    }
+    public int getNumberOfSeats() {
+        return numberOfSeats;
+    }
+    @Override
+    public void start() {
+        System.out.println("Vehicle  stalled");
+    }
+    @Override
+    public void stop() {
+        System.out.println("Vehicle hasn't started");
+    }
 }
